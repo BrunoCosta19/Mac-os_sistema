db4d13fc6bf6d8cd9f1001d6db7a39a31afc0fb6
Bruno Costa <102918628+BrunoCosta19@users.noreply.github.com>
2023-06-30 19:09:14 -0300

    Desktop Mac os
    projeto de fazer uma copia da area de trabalho do Mac

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', function(event) {
+    // Perform some task
+    // console.log('OK!!!');
+  });
+  
+  self.addEventListener('activate', function(event) {
+    // Perform some task
+    // console.log('Active!!!');
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    
+  });
+  
